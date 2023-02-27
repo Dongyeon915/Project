7ed787736914f9a3f1a0245c90b9a0a1c5de17da
dongyeonkim <ehd9567@naver.com>
2023-02-27 18:36:03 +0900

    repository space-between으로 간격 조절

--- src/components/repositories/RepositoriesComp.jsx
+++ src/components/repositories/RepositoriesComp.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardContent,
@@ -16,39 +17,39 @@ function repositoriesComp() {
       <Grid2>
         {repositoryWebhooks.map((repository, index) => {
           return (
-                <Paper elevation={3}>
-                  <Card sx={{marginTop: 2}}>
-                    <CardContent>
-                      <Typography>
-                        {repositoryWebhooks.name}
-                      </Typography>
-                      <Stack direction={"row"} justifyContent={"space-between"}>
-                        <Link
-                            href={repository.html_url2}
-                            sx={{textDecoration: "none", color: "#333333"}}
-                        >
-                          <Typography variant={"body1"}>
-                            <GitHub sx={{marginRight: 2}}/>
-                            {repository.rePoInfo}
-                          </Typography>
-                        </Link>
-                        <Typography variant={"body2"}>
-                          업데이트 :
-                          {repository.updated_at}
+              <Paper elevation={3}>
+                <Card sx={{marginTop: 2}}>
+                  <CardContent>
+                    <Stack direction={"row"} justifyContent={"space-between"}>
+                      <Link
+                          href={repository.html_url2}
+                          sx={{textDecoration: "none", color: "#333333"}}
+                      >
+                        <Typography variant={"body1"}>
+                          <GitHub sx={{marginRight: 2}}/>
+                          {repository.rePoInfo}
                         </Typography>
-                      </Stack>
-                      <Grid2 container={"true"}>
-                        <Stack>
-                          sdfsdf
-                        </Stack>
-                        <Grid2 spacing={1} marginLeft={105}>
-                          <Button variant="outlined"
-                                  size={"small"}>show Commits</Button>
-                        </Grid2>
+                      </Link>
+                      <Typography variant={"body2"}>
+                        업데이트 :
+                        {repository.updated_at}
+                      </Typography>
+                    </Stack>
+
+
+
+                    <Stack direction={"row"} justifyContent={"space-between"}>
+                      <Grid2 padding={1}>
+                          {repository.language}
                       </Grid2>
-                    </CardContent>
-                  </Card>
-                </Paper>
+                      <Grid2>
+                        <Button variant="outlined"
+                                size={"small"}>show Commits</Button>
+                      </Grid2>
+                    </Stack>
+                  </CardContent>
+                </Card>
+              </Paper>
           )
         })}
 
