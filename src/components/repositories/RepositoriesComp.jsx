b0bd19c5b3fafc36bcd508aab114b9ceff3faf29
dongyeonkim <ehd9567@naver.com>
2023-02-27 14:20:47 +0900

    commit버튼 추가

--- src/components/repositories/RepositoriesComp.jsx
+++ src/components/repositories/RepositoriesComp.jsx
@@ -1,4 +1,12 @@
-import {Card, CardContent, Link, Paper, Stack, Typography} from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Link,
+  Paper,
+  Stack,
+  Typography
+} from "@mui/material";
 import repositoryWebhooks from "./RepositoryWebhooks";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import {GitHub} from "@mui/icons-material";
@@ -11,34 +19,36 @@ function repositoriesComp() {
               <Paper elevation={3}>
                 <Card sx={{marginTop: 2}}>
                   <CardContent>
-
                     <Typography>
                       {repositoryWebhooks.name}
                     </Typography>
                     <Stack direction={"row"} justifyContent={"space-between"}>
                       <Link
-
                           href={repository.html_url2}
                           sx={{textDecoration: "none", color: "#333333"}}
                       >
-                          <Typography variant={"h6"}>
-                            <GitHub sx={{marginRight:2}}/>
-                            {repository.rePoInfo}
-                          </Typography>
+                        <Typography variant={"h6"}>
+                          <GitHub sx={{marginRight: 2}}/>
+                          {repository.rePoInfo}
+                        </Typography>
                       </Link>
                       <Typography variant={"body2"}>
                         업데이트 :
                         {repository.updated_at}
                       </Typography>
                     </Stack>
+                    <Stack direction={"row-reverse"} spacing={1}>
+                      <Button variant="outlined"
+                              size={"small"}>show Commits</Button>
+                    </Stack>
                   </CardContent>
                 </Card>
               </Paper>
-
           )
         })}
+
       </Grid2>
   )
-
 }
-  export default repositoriesComp
+
+export default repositoriesComp
