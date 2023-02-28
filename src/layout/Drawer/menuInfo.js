f4753b65a0853c06bafcce20e5746c59c9fdace8
dongyeonkim <ehd9567@naver.com>
2023-02-28 20:52:06 +0900

    라우터 path경로 js

---
+++ src/layout/Drawer/menuInfo.js
@@ -0,0 +1,16 @@
+export const menuInfo = [
+  {
+    id: 1,
+    name: "Overview",
+    link: "/overview"
+  },
+  {
+    id: 2,
+    name: "Repositories",
+    link: "/repository",
+  },
+  {
+    name: "Schedule",
+    link: "/today"
+  }
+]
