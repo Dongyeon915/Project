2b9fc2250cb04a2a9f49e0441125f3de9b34c8e8
dongyeonkim <ehd9567@naver.com>
2023-02-28 20:53:03 +0900

    라우터 module

---
+++ src/router/Router.jsx
@@ -0,0 +1,36 @@
+import {createBrowserRouter} from "react-router-dom";
+import App from "../App";
+import OverviewComp from "../components/overview-comp/OverviewComp";
+import RepositoriesComp from "../components/repositories/RepositoriesComp";
+import Schedule from "../components/Schedule";
+import TodayComp from "../components/today/TodayComp";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <OverviewComp/>
+      },
+      {
+        path: "/overview",
+        element: <OverviewComp/>
+      },
+      {
+        path: "/repository",
+        element: <RepositoriesComp/>
+      },
+      {
+        path: "/timer",
+        element: <Schedule/>
+      },
+      {
+        path: "/today",
+        element: <TodayComp/>
+      }
+    ]
+  }
+]);
