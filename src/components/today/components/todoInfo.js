b3b15d25407e33caa10caabeb7ba47998c1a66fd
dongyeonkim <ehd9567@naver.com>
2023-03-02 17:15:45 +0900

    todo의 test정보 js

---
+++ src/components/today/components/todoInfo.js
@@ -0,0 +1,16 @@
+export const todoInfo = {
+  todayList : [
+    {
+      id: 1,
+      task: "씻기",
+      complete: false,
+      completeTime: ''
+    },
+    {
+      id: 2,
+      task: "밥먹기",
+      complete: false,
+      completeTime: ''
+    }
+  ]
+}
