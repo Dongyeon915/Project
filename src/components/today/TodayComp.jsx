d85a4e086b7a374a2427b71dceab0ce5cca17e2c
dongyeonkim <ehd9567@naver.com>
2023-03-04 00:07:15 +0900

    TodayComp 내부에 TaskComp 모듈화

--- src/components/today/TodayComp.jsx
+++ src/components/today/TodayComp.jsx
@@ -1,22 +1,11 @@
 import Grid2 from "@mui/material/Unstable_Grid2";
-import {
-  Checkbox,
-  Container,
-  Divider,
-  IconButton,
-  Input,
-  Paper,
-  Snackbar,
-  Stack,
-  Typography
-} from "@mui/material";
-import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import {Container} from "@mui/material";
 import {useState} from "react";
-import TodayComp from "./TodayComp";
+import TaskComp from "./TaskComp";
 
 const localStorage = window.localStorage;
 
-export default function TodayCompExample() {
+export default function TodayComp() {
 
   const [state, setState] = useState({
     open: false,
@@ -111,63 +100,8 @@ export default function TodayCompExample() {
           {/*  </Paper>*/}
           {/*</Grid2>*/}
         </Grid2>
-
-        <Grid2 container="true" spacing={2}>
-          <Grid2 lg={6} sx={{maxHeight: 500}}>
-            <Paper variant={"elevation"} elevation={4} sx={{
-              padding: 3,
-              height: "100%",
-              display: "flex",
-              flexDirection: "column"
-            }}>
-              <Typography variant={"h6"} fontWeight={700}>Todo List</Typography>
-              <Typography
-                  variant={"subtitle2"}>{new Date().toLocaleDateString()}</Typography>
-              <Divider sx={{marginTop: 1, backgroundColor: '#333'}}/>
-              <Stack marginTop={3} marginBottom={3} flexGrow={1}
-                     sx={{overflowY: "auto"}} spacing={2}>
-                {
-                  state.todoList.map((todo, index) => {
-                    return (
-                        <Paper variant={"outlined"} elevation={1} sx={{
-                          padding: "10px 20px",
-                          display: "flex",
-                          alignItems: "center"
-                        }}>
-                          <Checkbox checked={todo.complete} onChange={(e) => {
-                            todoComplete(e, index)
-                          }} defaultChecked color="secondary"/>
-                          {
-                            todo.complete ? <Typography
-                                    sx={{textDecoration: "line-through"}}>{todo.task} {todo.completeTime}</Typography>
-                                :
-                                <Typography>{todo.task}</Typography>
-                          }
-                        </Paper>
-                    )
-                  })
-                }
-              </Stack>
-              <Snackbar
-                  open={state.open}
-                  autoHideDuration={3000}
-                  onClose={handleClose}
-                  message="You are so Nice!"
-              />
-              <Stack direction={"row"} alignItems={"center"} sx={{
-                backgroundColor: "#4E545C",
-                padding: '3px 20px',
-                borderRadius: 5
-              }}>
-                <Input value={state.todoInput} onChange={todoInputHandler}
-                       placeholder="New Task..."
-                       sx={{flexGrow: 1, color: '#fff'}}/>
-                <IconButton onClick={buttonEvent}>
-                  <PlaylistAddIcon fontSize={"medium"} sx={{color: "#fff"}}/>
-                </IconButton>
-              </Stack>
-            </Paper>
-          </Grid2>
+        <Grid2 container="true" spacing={2} sm={12}>
+          <TaskComp/>
         </Grid2>
       </Container>
   )
