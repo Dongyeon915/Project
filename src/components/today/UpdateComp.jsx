6fb372738bec6ee696004efc385c49d03dd344a4
dongyeonkim <ehd9567@naver.com>
2023-03-04 16:33:52 +0900

    update창 임시 모듈화

--- src/components/today/UpdateComp.jsx
+++ src/components/today/UpdateComp.jsx
@@ -1,12 +1,21 @@
-import {Paper} from "@mui/material";
+import {Box, Input, Paper, Stack, Typography} from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import Divider from "@mui/material/Divider";
 
 export default function UpdateComp() {
   return (
-      <Grid2 zIndex={100}>
-        <Paper>
-          <p>업데이트창</p>
+      <Stack direction={"row"} alignItems={"center"}
+             justifyContent={"space-between"} flexGrow={1}
+             position={"absolute"}
+             zIndex={1000}
+      >
+        <Paper sx={{minWidth:400,position : "absolute"}} >
+          <Typography variant={"h5"} flexGrow={1}>
+            Edit
+          </Typography>
+          <Divider/>
+          <Input placeholder={"수정할 내용을 입력해주세요."} sx={{minWidth:400}}/>
         </Paper>
-      </Grid2>
+      </Stack>
   )
 }
