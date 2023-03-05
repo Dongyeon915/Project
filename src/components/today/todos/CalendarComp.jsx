a1f26e26fd91a9b22b5c31b5ad5f33ef60eb4fd3
dongyeonkim <ehd9567@naver.com>
2023-03-05 22:30:15 +0900

    CalendarComp 내용수정전

--- src/components/today/todos/CalendarComp.jsx
+++ src/components/today/todos/CalendarComp.jsx
@@ -7,32 +7,31 @@ export default function CalendarComp(){
       <Calendar
           minDetail={"year"}
           defaultValue={new Date()}
+
           onClickDay={(value, event) => {
-            console.log("onClickDay")
-            console.log(value)
           }}
-          tileContent={
-            (dateInfo) => {
-              const date = dateInfo.date
-              if (date.getDate() === 4) {
-                return (
-                    <Box sx={{paddingTop: 2}}>
-                      <CircularStatic progress={70}/>
-                    </Box>
-                )
-              } else if (date.getDate() < 4) {
-                return (
-                    <Box sx={{paddingTop: 2}}>
-                      <CircularStatic progress={10}/>
-                    </Box>
-                )
-              } else {
-                return (
-                    <Box sx={{paddingTop: 2, height: "50px"}}></Box>
-                )
-              }
-            }
-          }
+          // tileContent={
+          //   (dateInfo) => {
+          //     const date = dateInfo.date
+          //     if (date.getDate() === 4) {
+          //       return (
+          //           <Box sx={{paddingTop: 2}}>
+          //             <CircularStatic progress={70}/>
+          //           </Box>
+          //       )
+          //     } else if (date.getDate() < 4) {
+          //       return (
+          //           <Box sx={{paddingTop: 2}}>
+          //             {/*<CircularStatic progress={10}/>*/}
+          //           </Box>
+          //       )
+          //     } else {
+          //       return (
+          //           <Box sx={{paddingTop: 2, height: "50px"}}></Box>
+          //       )
+          //     }
+          //   }
+          // }
       />
   )
 }
