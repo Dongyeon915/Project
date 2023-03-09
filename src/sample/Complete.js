648f8954cb85e96f74a0d4c4c32c3ce3f09342db
dongyeonkim <ehd9567@naver.com>
2023-03-09 18:40:49 +0900

    Application 전반적인 Data 형식 정의

---
+++ src/sample/Complete.js
@@ -0,0 +1,36 @@
+export const response = {
+  dayInfo: {
+    date: "2023-03-09",
+    todo: {
+      result: {
+        clear: 0,
+        rest: 0,
+      },
+      list: [
+        {
+          taskId: 1,
+          task: "씻기",
+          complete: false,
+          completeTime: undefined
+        }
+      ]
+    },
+    pomodoro: {
+      result: {
+        interval: 0
+      },
+      config: {
+        minute: 15,
+        rest: 5
+      },
+      timer: {
+        state: {
+          isRest: true,
+          isRunning: false,
+          isPause: false,
+        },
+        count: 15 * 60
+      }
+    }
+  }
+}
