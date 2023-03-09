e482df7ca64d482e88d50a3d21917e84c148904b
dongyeonkim <ehd9567@naver.com>
2023-03-09 18:40:04 +0900

    Todo Sample JSON 정의

---
+++ src/sample/taskSample.js
@@ -0,0 +1,14 @@
+export const taskSample = {
+  todoInput: "",
+  clearTask: 0,
+  restTask: 0,
+  todoList: [
+    {
+      taskId: 1,
+      created : "2023-03-05",
+      task: "씻기",
+      complete: false,
+      completeTime: undefined
+    }
+  ]
+}
