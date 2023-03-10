16a3da0c55b92c8bd2d817078d0ad9f655f0baa4
dongyeonkim <ehd9567@naver.com>
2023-03-10 14:45:45 +0900

    TaskComp 함수정의

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,18 +1,16 @@
-import {ADD_TODO} from "../actions/todoAction";
+import {
+  ADD_TODO,
+  COMPLETE_TODO,
+  DELETE_TODO,
+  UPDATE_TODO
+} from "../actions/todoAction";
 
 const initialState = {
   result: {
     clear: 0,
     rest: 0,
   },
-  list: [
-    {
-      taskId: 1,
-      task: "씻기",
-      complete: false,
-      completeTime: undefined
-    }
-  ]
+  list: []
 }
 
 // 초기값이 없다면 initialStat 값으로 세팅
@@ -25,7 +23,7 @@ export default function todoReducer(state = initialState, action) {
         rest: state.result.rest + 1
       },
       list: [
-          ...state.list,
+        ...state.list,
         {
           taskId: state.list.length + 1,
           task: action.task,
@@ -34,6 +32,60 @@ export default function todoReducer(state = initialState, action) {
         }
       ]
     }
+  } else if (action.type == COMPLETE_TODO) {
+    let isComplete = false
+    const todoList = state.list.filter(task => {
+      if(task.taskId === action.taskID) {
+        task.complete = !task.complete
+        task.completeTime = new Date().toLocaleTimeString()
+        isComplete = task.complete
+      }
+      return task
+    })
+    let restCount = state.result.rest, clearCount = state.result.clear
+    if (isComplete) {
+      restCount--
+      clearCount++
+    } else {
+      restCount++
+      clearCount--
+    }
+    return {
+      ...state,
+      result: {
+        clear: clearCount,
+        rest: restCount
+      },
+      list: todoList
+    }
+  } else if (action.type == DELETE_TODO) {
+    // event를 todoItem으로 읽기 좋게 변경 사용
+    const todoList = state.list.filter(task => {
+      if (task.taskId !== action.taskID) {
+        return task
+      }
+    })
+    return {
+      ...state,
+      result: {
+        ...state.result,
+        rest: state.result.rest - 1
+      },
+      list: todoList
+    }
+  } else if (action.type == UPDATE_TODO) {
+    const updateTodoList = state.list.filter((task) => {
+      if (task.taskId == action.taskID) {
+        task.task = action.updateTask
+        return task
+      }
+      return task
+    })
+    return {
+      ...state,
+      list: updateTodoList
+    }
   }
+  // 없는 액션타입은 꼭 state로 내보내 줘야한다.
   return state
 }
