07e5ee5c79fdbf28cf1850ea62d95f848487d8ce
dongyeonkim <ehd9567@naver.com>
2023-03-10 14:44:54 +0900

    redux root경로combineReducers생성

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import todoReducer from "./todoReducer";
-
+import pomodoroReducer from "./pomodoroReducer";
+// 하나의 리듀서를 교체할수 없으니 combineReducers를 store에넣어줘야함
 export const rootReducer = combineReducers({
-  todo: todoReducer
+  todo: todoReducer,
+  pomodoro: pomodoroReducer
 })
