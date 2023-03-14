b55845cb1ad0830c731a8e928f094c623075d34c
dongyeonkim <ehd9567@naver.com>
2023-03-14 22:12:20 +0900

    pomodoroReducer 로직 수정

--- src/redux/reducers/test-pomodoro-reducer/pomodoroReducer.js
+++ src/redux/reducers/test-pomodoro-reducer/pomodoroReducer.js
@@ -1,55 +1,103 @@
-import {RESET_TIMER, START_TIMER} from "../../actions/pomodoroAction";
+import {
+  CHANGE_RUNNING_STATE,
+  COUNT_DOWN, FUCK_YOU,
+  PAUSE_TIMER,
+  RESET_TIMER,
+  RUN_TIMER,
+  SET_COUNT_DOWN,
+  SET_MINUTE,
+  SET_PAUSE_STATE,
+  SET_RUNNING_STATE,
+  START_TIMER
+} from "../../actions/pomodoroAction";
 
 export const pomodoroInitialState = {
-  pomodoro: {
-    result: {
-      interval: 0
-    },
-    config: {
-      minute: 0,
-      rest: 0
-    },
-    timer: {
-      state: {
-        isRest: true,
-        isRunning: false,
-        isPause: false,
-      },
-      countValue: 0,
+  result: {
+    interval: 0
+  },
+  config: {
+    minute: 1,
+    rest: 10,
+    countValue: 0
+  },
+  timer: {
+    state: {
+      isRest: true,
+      isRunning: false,
+      isPause: false,
     }
   }
 }
 
 
-
-export default function pomodoroReducer(state = pomodoroInitialState,action){
-  if (action.type === START_TIMER && action.timerReference.current) {
-      console.log("Timer Already Exist!")
-      return;
+export default function pomodoroReducer(state = pomodoroInitialState, action) {
+  if (action.type === SET_RUNNING_STATE) {
+    return {
+      ...state,
+      timer: {
+        ...state.timer,
+        state: {
+          ...state.timer.state,
+          isRest: false,
+          isRunning: true,
+          isPause: false
+        }
+      }
     }
-    // --------------------------------------------------
-      if (state.pomodoro.timer.isPause) {
-        return {
-          ...state,
-          timer: {
-            ...state.pomodoro.timer,
-            isPause: true,
-            isRest: state.pomodoro.timer.state.isRest
-          }
-        }
-      } else {
-        state.pomodoro.timer.state.isPause = true
-        state.pomodoro.timer.state.isRestState = !state.pomodoro.timer.state.isRest
+  } else if (action.type === SET_MINUTE) {
+    return {
+      ...state,
+      config: {
+        ...state.config,
+        minute: action.minute
       }
-      return {
-        ...state,
-        timer: {
-          ...state.pomodoro.timer,
-          isRest: state.pomodoro.timer.state.isRest,
-          isRunning: !state.pomodoro.timer.state.isRunning,
-          isPause: state.pomodoro.timer.state.isPause
+    }
+  }else if (action.type === RUN_TIMER) {
+    return {
+      ...state,
+      config: {
+       ...state.config,
+       countValue: state.config.countValue - 1
+      },
+      timer: {
+        ...state.timer
+      }
+    }
+  } else if (action.type === SET_COUNT_DOWN){
+    return {
+      ...state,
+      config: {
+        ...state.config,
+        countValue: action.countDown
+      }
+    }
+  }else if (action.type === CHANGE_RUNNING_STATE){
+    return {
+      ...state,
+      timer: {
+        ...state.timer,
+        isRunning: true,
+        isPause: false
+      }
+    }
+  } else if (action.type === SET_PAUSE_STATE){
+    return {
+      ...state,
+      timer: {
+        ...state.timer,
+        state: {
+          ...state.timer.state,
+          isPause: true,
+          isRunning: false
         }
       }
-  return state
+    }
+  } else if (action.type === FUCK_YOU) {
+    return {
+      ...state,
+      userInfo: action.userInfo
+    }
   }
-    // ---------------------------------------------------
+
+  return state
+}
