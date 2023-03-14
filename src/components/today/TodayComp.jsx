43a371998fa8052382bf10a2bba27eb475707865
dongyeonkim <ehd9567@naver.com>
2023-03-14 22:10:04 +0900

    라인정리

--- src/components/today/TodayComp.jsx
+++ src/components/today/TodayComp.jsx
@@ -1,32 +1,45 @@
 import Grid2 from "@mui/material/Unstable_Grid2";
-import {Container, Paper} from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Container,
+  Paper,
+  Typography
+} from "@mui/material";
 import TodoComp from "./todos/TodoComp";
 import "../../css/calendar.css";
 import CalendarComp from "./todos/CalendarComp";
-import PomodoroComp from "./pomodoro/test-pomodoro/PomodoroComp";
-import PomodoroCopy from "./pomodoro/PomodoroCopy";
+import PomodoroComp from "./pomodoro/test-pomodoro/PomodoroComp"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function TodayComp() {
 
   return (
 
-      <Container fixed={"true"}>
+      <Container fixed={"true"} sx={{backgroundColor:"#EE5D50FF",borderRadius:8}}>
         <Grid2 container={"true"} lg={12} spacing={2}>
           <Grid2 lg={12}>
-            <Paper variant={3}>
-              <PomodoroCopy/>
-            </Paper>
+            <Accordion sx={{backgroundColor:"#3CB371"}}>
+              <AccordionSummary
+                  expandIcon={<ExpandMoreIcon/>}
+              >
+                <Typography color={"white"} variant={"h5"}>Calendar</Typography>
+              </AccordionSummary>
+              <AccordionDetails >
+                <CalendarComp/>
+              </AccordionDetails>
+            </Accordion>
           </Grid2>
         </Grid2>
-
         {/*today 하단 TodoTask,Calendar*/}
-        <Grid2 container="true" spacing={2} sm={12}>
-          <Grid2 sm={6}>
+        <Grid2 container="true" spacing={1} sm={12}>
+          <Grid2 sm={6} padding={1}>
             <TodoComp/>
           </Grid2>
           <Grid2 sm={6} padding={2}>
             <Paper variant={"elevation"} elevation={3}>
-              <CalendarComp/>
+              <PomodoroComp/>
             </Paper>
           </Grid2>
         </Grid2>
