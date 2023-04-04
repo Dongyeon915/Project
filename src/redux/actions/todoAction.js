6234e091179f0249024a5693bdf542be5ab90374
dongyeonkim <ehd9567@naver.com>
2023-04-04 19:51:24 +0900

    rest와 clear사용시 액션정의

--- src/redux/actions/todoAction.js
+++ src/redux/actions/todoAction.js
@@ -4,6 +4,9 @@ export const DELETE_TODO = "DELETE_TODO"
 export const UPDATE_TODO = "UPDATE_TODO"
 export const GET_ALL_TODO = "GET_ALL_TODO"
 
+// clear와 rest를가져온다
+export const GET_USER_RSULT = "GET_USER_RSULT"
+
 // 체크 박스 업데이트 이벤트
 export const UPDATE_CHECKBOX = "UPDATE_CHECKBOX"
 
@@ -49,5 +52,12 @@ export function checkBoxTodoActionCreator(updateTodo){
   return {
     type: UPDATE_CHECKBOX,
     checkBoxupdate: updateTodo
+  }
+}
+
+export function getUserResult(result){
+  return {
+    type: GET_USER_RSULT,
+    userResult: result
   }
 }
