6dce084a080254a7d22a743ef21efd3f8bb7cf7d
dongyeonkim <ehd9567@naver.com>
2023-04-08 16:28:41 +0900

    캘린더 연습

--- src/redux/reducers/calendarReducer.js
+++ src/redux/reducers/calendarReducer.js
@@ -1,22 +1,29 @@
 import {GET_CALENDAR_BY_USER} from "../actions/calendarAction";
 
 const initialState = {
-    userId: null,
-    date: null,
-    clear: 0,
-    rest: 0
+  list:[]
+      // userId: null,
+      // date: null,
+      // clear: 0,
+      // rest: 0,
 }
 
 
 
 export default function todoResultReducer(state = initialState, action) {
   if (action.type === GET_CALENDAR_BY_USER) {
+    const arr = []
+    action.userResult.forEach(result => {
+      arr.push({
+        date: result.date,
+        rest: result.rest_task,
+        clear: result.clear_task,
+        userId: result.user_id
+      })
+    })
       return {
         ...state,
-        date: action.userResult.date,
-        rest: action.userResult.rest_task,
-        clear: action.userResult.clear_task,
-        userId: action.userResult.user_id
+        list: arr
     }
   }
   return state
