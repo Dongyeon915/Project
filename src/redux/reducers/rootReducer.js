2575dacdc22080a75617fa45a5c3e4ae75190853
dongyeonkim <ehd9567@naver.com>
2023-05-06 21:40:35 +0900

    rootReducer login:loginReduser정의

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,11 +3,12 @@ import pomodoroReducer from "./pomodoroReducer";
 import todoReducer from "./todoReducer";
 import todoResultReducer from "./calendarReducer";
 import tilReducer from "./tilReducer";
+import loginReducer from "./loginReducer";
 // 하나의 리듀서를 교체할수 없으니 combineReducers를 store에넣어줘야함
 export const rootReducer = combineReducers({
   todo: todoReducer,
   pomodoro: pomodoroReducer,
   todoResult: todoResultReducer,
-  til:tilReducer
-
+  til:tilReducer,
+  login: loginReducer
 })
