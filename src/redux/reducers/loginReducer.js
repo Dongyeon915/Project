d15315b74c1f3ab1e2d92c775cf613f588a23445
dongyeonkim <ehd9567@naver.com>
2023-05-08 20:04:45 +0900

    token저장 state설정

--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -2,7 +2,7 @@ import {GET_LOGIN_INFO} from "../actions/loginAction";
 
 const initialState = {
   isAuthenticated: false,
-  access_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLrj5nsl7AiLCJwcm9maWxlIjoiaHR0cDovL2sua2FrYW9jZG4ubmV0L2RuL2RwazlsMS9idHFtR2hBMmxLTC9PejB3RHVKbjFZVjJESW45MmY2RFZLL2ltZ182NDB4NjQwLmpwZyIsImlkIjoiMjcyMTc1MDI0OCIsImV4cCI6MTY4MzM3NzI3MywiZW1haWwiOiJlaGQ5NTY3QG5hdmVyLmNvbSJ9.GP3V2QAee_gxQn_SG5cF9HHfNt8T4nsYJWZepcNgfs0XUMtJXSgk62w6-6xR1lsQ6SChydGHQtpOzfl-r2T-gg",
+  access_token: null,
   refresh_token: null
 }
 
@@ -10,6 +10,9 @@ export default function loginReducer(state = initialState, action) {
   if (action.type === GET_LOGIN_INFO) {
       return {
         ...state,
+        isAuthenticated: true,
+        access_token: action.access_token,
+        refresh_token: action.refresh_token
     }
   }
   return state
