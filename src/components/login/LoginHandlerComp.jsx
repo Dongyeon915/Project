e0d09153a170b2ed32d8565cff33be11af619dae
dongyeonkim <ehd9567@naver.com>
2023-05-09 21:49:58 +0900

    useEffect수정

--- src/components/login/LoginHandlerComp.jsx
+++ src/components/login/LoginHandlerComp.jsx
@@ -2,28 +2,47 @@ import {Container} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import {useParams, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
+import {myRequestGenerator} from "../../helper/helper";
+import {useDispatch, useSelector} from "react-redux";
+import {getLoginInfoAction} from "../../redux/actions/loginAction";
 
 export default function LoginHandlerComp() {
 
   const params = useParams();
   const navigate = useNavigate();
 
+  // 동연
+  const login = useSelector(state => state.login)
+  const dispatch = useDispatch();
+
+
   useEffect(() => {
-    console.log(params)
-    console.log(params.access_token)
-    console.log(params.refresh_token)
+    // console.log(params)
+    // console.log(params.access_token)
+    // console.log(params.refresh_token)
     localStorage.setItem("access_token", params.access_token)
     localStorage.setItem("refresh_token", params.refresh_token)
-    // redux
-    // {
-    //   isAuthenticated: true,
-    //   access_token: "********",
-    //   refresh_token: "********"
-    // }
-    //
-    //
+
+    // 동연
+    const refresh_token = localStorage.getItem("refresh_token");
+    const access_token = localStorage.getItem("access_token");
+    // console.log("저장되는지 확인" + refresh_token);
+    // console.log("저장되는지 확인" + accesstoken);
+
+    // fetch(myRequestGenerator(`/`),{
+    //   method:"POST",
+    //   headers: {
+    //     Authentication: `Bearer ${accesstoken}`
+    //   },}).then(response => response.json()).then(user => {
+    //     console.log("유저확인" + user)
+    //     dispatch(getLoginInfoAction(user.access_token,user.refresh_token))
+    // }).catch(error => {
+    //   console.log(error)
+    // })
+    dispatch(getLoginInfoAction(params.access_token ,params.refresh_token));
     navigate("/overview")
-  })
+  },[])
+
 
   return (
       <Container maxWidth="false">
