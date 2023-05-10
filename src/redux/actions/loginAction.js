b686173978c5a8fe23eda33262d3c38198f9e862
dongyeonkim <ehd9567@naver.com>
2023-05-10 20:59:40 +0900

    user정보를 저장하는 액션 수정

--- src/redux/actions/loginAction.js
+++ src/redux/actions/loginAction.js
@@ -1,9 +1,10 @@
 export const GET_LOGIN_INFO = "GET_LOGIN_INFO"
 
-export function getLoginInfoAction(access_token,refresh_token){
+export function getLoginInfoAction(access_token,refresh_token, userInfo){
   return {
     type: GET_LOGIN_INFO,
     access_token:access_token,
-    refresh_token:refresh_token
+    refresh_token:refresh_token,
+    userInfo: userInfo
   }
 }
