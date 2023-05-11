fe9fc1c5a617a35b1e57de801b19082c71f1584f
dongyeonkim <ehd9567@naver.com>
2023-05-11 14:42:30 +0900

    url 비활성화

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,4 +1,6 @@
-export function myRequestGenerator(path) {
-  // http://localhost:8080/schedule
-  return `${process.env.REACT_APP_BASE_SERVER_URL}${path}`
-}
+// export function myRequestGenerator(path) {
+//   // http://localhost:8080/schedule
+//   return `${process.env.REACT_APP_BASE_SERVER_URL}${path}`
+// } 
+
+// 사용중은 아니지만 확인용 보관
